4ea1a95180a42cb1039908cde93bb06af3afe5b1
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-07-10 22:31:41 +0100

    Split ex code into components

--- app.jsx
+++ app.jsx
@@ -1,6 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
 class App extends React.Component {
   render() {
     return (
@@ -26,26 +23,7 @@ class App extends React.Component {
           </nav>
         </header>
         <main>
-          <div className="product-carousel">
-            <ul className="carousel-container">
-              <li id="cb0" className="carousel-big show"><img src="images/image-product-1.jpg" /></li>
-              <li id="cb1" className="carousel-big"><img src="images/image-product-2.jpg" /></li>
-              <li id="cb2" className="carousel-big"><img src="images/image-product-3.jpg" /></li>
-              <li id="cb3" className="carousel-big"><img src="images/image-product-4.jpg" /></li>
-              <button className="carousel-container__button previous">
-                <img src="images/icon-previous.svg" />
-              </button>
-              <button className="carousel-container__button next">
-                <img src="images/icon-next.svg" />
-              </button>
-            </ul>
-            <ul className="carousel-menu">
-              <li className="carousel-thumb active"><img src="images/image-product-1-thumbnail.jpg" /></li>
-              <li className="carousel-thumb"><img src="images/image-product-2-thumbnail.jpg" /></li>
-              <li className="carousel-thumb"><img src="images/image-product-3-thumbnail.jpg" /></li>
-              <li className="carousel-thumb"><img src="images/image-product-4-thumbnail.jpg" /></li>
-            </ul>
-          </div>
+          <ProductCarousel />
           <div className="product-information">
             <h3 className="product-information__brand">Sneaker Company</h3>
             <h1>Fall Limited Edition Sneakers</h1>
@@ -56,14 +34,7 @@ class App extends React.Component {
               <span className="pricing__discount">50%</span>
               <span className="pricing__price-old">$250.00</span>
             </div>
-            <div className="purchase">
-              <div className="purchase-number">
-                <button className="purchase-number__adjust" onClick="this.parentNode.querySelector('input[type=number]').stepDown()"><img src="images/icon-minus.svg" /></button>
-                <input className="purchase-number__display" type="number" min="1" value="1" />
-                <button className="purchase-number__adjust" onClick="this.parentNode.querySelector('input[type=number]').stepUp()"><img src="images/icon-plus.svg" /></button>
-              </div>
-              <button id="add-to-cart">Add to cart</button>
-            </div>
+            <Purchase />
           </div>
         </main>
         <footer className="attribution">
@@ -71,6 +42,56 @@ class App extends React.Component {
           Coded by <a href="https://github.com/empowermint">James T. Harding</a>.
         </footer>
       </>
+    )
+  }
+}
+
+class ProductCarousel extends React.Component {
+  render() {
+    return (
+      <div className="product-carousel">
+        <ul className="carousel-container">
+          <li id="cb0" className="carousel-big show"><img src="images/image-product-1.jpg" /></li>
+          <li id="cb1" className="carousel-big"><img src="images/image-product-2.jpg" /></li>
+          <li id="cb2" className="carousel-big"><img src="images/image-product-3.jpg" /></li>
+          <li id="cb3" className="carousel-big"><img src="images/image-product-4.jpg" /></li>
+          <button className="carousel-container__button previous">
+            <img src="images/icon-previous.svg" />
+          </button>
+          <button className="carousel-container__button next">
+            <img src="images/icon-next.svg" />
+          </button>
+        </ul>
+        <CarouselMenu />
+      </div>
+    )
+  }
+}
+
+class CarouselMenu extends React.Component {
+  render() {
+    return (
+      <ul className="carousel-menu">
+        <li className="carousel-thumb active"><img src="images/image-product-1-thumbnail.jpg" /></li>
+        <li className="carousel-thumb"><img src="images/image-product-2-thumbnail.jpg" /></li>
+        <li className="carousel-thumb"><img src="images/image-product-3-thumbnail.jpg" /></li>
+        <li className="carousel-thumb"><img src="images/image-product-4-thumbnail.jpg" /></li>
+      </ul>
+    )
+  }
+}
+
+class Purchase extends React.Component {
+  render() {
+    return (
+      <div className="purchase">
+        <div className="purchase-number">
+          <button className="purchase-number__adjust"><img src="images/icon-minus.svg" /></button>
+          <input className="purchase-number__display" type="number" min="1" defaultValue="1" />
+          <button className="purchase-number__adjust"><img src="images/icon-plus.svg" /></button>
+        </div>
+        <button id="add-to-cart">Add to cart</button>
+      </div>
     )
   }
 }
